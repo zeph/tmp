b03a66ec82062a344c0c0bbc3b8856b56dc582e0
Guido Serra aka Zeph <zeph@fsfe.org>
2011-03-25 01:50:56 +0100

    better pretend the credentials

--- k33p-me/helloworld.py
+++ k33p-me/helloworld.py
@@ -8,9 +8,11 @@ from google.appengine.ext import blobstore
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import login_required
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainHandler(webapp.RequestHandler):
+    @login_required
     def get(self):
         upload_url = blobstore.create_upload_url('/upload')
         self.response.out.write('<html><body>')
@@ -19,12 +21,14 @@ class MainHandler(webapp.RequestHandler):
             name="submit" value="Submit"> </form></body></html>""")
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    @login_required
     def post(self):
         upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
         blob_info = upload_files[0]
         self.redirect('/serve/%s' % blob_info.key())
 
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    @login_required
     def get(self, resource):
         resource = str(urllib.unquote(resource))
         blob_info = blobstore.BlobInfo.get(resource)
