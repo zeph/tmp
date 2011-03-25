da851522f8320fd61fa181d72c2e8e1c66f9ecc3
Guido Serra aka Zeph <zeph@fsfe.org>
2011-03-25 01:32:14 +0100

    placeholder

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,59 +1,43 @@
-import cgi
+#!/usr/bin/env python
+#
 
-from google.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext import db
 import os
+import urllib
+
+from google.appengine.ext import blobstore
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
 
-
-class Greeting(db.Model):
-    author = db.UserProperty()
-    content = db.StringProperty(multiline=True)
-    date = db.DateTimeProperty(auto_now_add=True)
-
-class MainPage(webapp.RequestHandler):
+class MainHandler(webapp.RequestHandler):
     def get(self):
-        greetings_query = Greeting.all().order('-date')
-        greetings = greetings_query.fetch(10)
+        upload_url = blobstore.create_upload_url('/upload')
+        self.response.out.write('<html><body>')
+        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+        self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit" 
+            name="submit" value="Submit"> </form></body></html>""")
 
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'greetings': greetings,
-            'url': url,
-            'url_linktext': url_linktext,
-        }
-
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
-
-
-class Guestbook(webapp.RequestHandler):
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
-        greeting = Greeting()
+        upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+        blob_info = upload_files[0]
+        self.redirect('/serve/%s' % blob_info.key())
 
-        if users.get_current_user():
-            greeting.author = users.get_current_user()
-
-        greeting.content = self.request.get('content')
-        greeting.put()
-        self.redirect('/')
-
-application = webapp.WSGIApplication(
-                                     [('/', MainPage),
-                                      ('/sign', Guestbook)],
-                                     debug=True)
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info)
 
 def main():
+    application = webapp.WSGIApplication(
+          [('/', MainHandler),
+           ('/upload', UploadHandler),
+           ('/serve/([^/]+)?', ServeHandler),
+          ], debug=True)
     run_wsgi_app(application)
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+  main()
 
