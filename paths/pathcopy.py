564670550e0aca3f05a4084ee42bef4e89c4606e
Guido Serra aka Zeph <zeph@fsfe.org>
2011-03-31 00:50:06 +0200

    input params

--- paths/pathcopy.py
+++ paths/pathcopy.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 import sys
 
+i = 0
+src_filename = ''
+dst_prefix = ''
+
+if len(sys.argv) < 5:
+    print 'Syntax: -f [filename] -d [destination_prefix]'
+    exit(1)
 for arg in sys.argv:
-    print arg
+    if arg == '-f':
+       src_filename = sys.argv[i+1]
+    if arg == '-d':
+       dst_prefix = sys.argv[i+1]
+    i = i+1
+
+print src_filename
+print dst_prefix
