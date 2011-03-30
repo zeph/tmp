e97b2b2d6760ee2f232922a91fa2854b1972e64f
Guido Serra aka Zeph <zeph@fsfe.org>
2011-03-31 01:01:52 +0200

    last modified

--- paths/pathcopy.py
+++ paths/pathcopy.py
@@ -21,4 +21,4 @@ print (src_filename, dst_prefix)
 f = open(src_filename, 'r')
 for raw_filename in f:
    filename = raw_filename.strip()
-   print (filename, time.ctime(os.path.getmtime(filename)))
+   print (filename, time.ctime(os.path.getmtime(filename)).strptime("%y-%m-%d"))
