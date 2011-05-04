f40a8921700ac40977b7ff7f55c456698077808f
Guido Serra aka Zeph <zeph@fsfe.org>
2011-05-04 08:41:38 +0200

    dumper for facebook

---
+++ facebook_dump.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# source:
+# http://santini.dsi.unimi.it/extras/ph/quick-and-dirty-backup-of-your-facebook-statuses.html
+
+from json import loads
+from urllib2 import urlopen
+from sys import argv
+
+next = 'https://graph.facebook.com/me/statuses?access_token=' + argv[ 1 ]
+while next:
+        url = urlopen( next )
+        data = loads( url.read() )
+        for msg in data[ 'data' ]:
+                print msg[ 'updated_time' ], msg[ 'message' ]
+        try:
+                next = data[ 'paging' ][ 'next' ]
+        except KeyError:
+                next = None
+
