385f7f53f4e1176fe11ce12bbef70e6c299acab6
Guido Serra <guido.serra@profitbricks.com>
2011-06-29 14:10:27 +0200

    playing around with pycharm+dojo

---
+++ dojo/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+# Copyright 2011 Guido Serra aka Zeph
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+
+# Set to true if we want to have our webapp print stack traces, etc
+_DEBUG = True
+
+
+# Add our custom Django template filters to the built in filters
+template.register_template_library('templatefilters')
+
+
+class BaseRequestHandler(webapp.RequestHandler):
+  """Supplies a common template generation function.
+
+  When you call generate(), we augment the template variables supplied with
+  the current user in the 'user' variable and the current webapp request
+  in the 'request' variable.
+  """
+  def generate(self, template_name, template_values={}):
+    values = {}
+    directory = os.path.dirname(__file__)
+    path = os.path.join(directory, os.path.join('templates', template_name))
+    self.response.out.write(template.render(path, values, debug=_DEBUG))
+
+class MainHandler(BaseRequestHandler):
+    def get(self):
+        self.generate('dojo.html')
+    # self.response.out.write('Hello world!')
+
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
